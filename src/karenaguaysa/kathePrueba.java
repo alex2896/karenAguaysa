d4309b361584d840a7b6452db7efd4f15ceaad32
daniel <>
2017-10-23 09:54:46 -0500

    Comentario en Kathe.

--- src/karenaguaysa/kathePrueba.java
+++ src/karenaguaysa/kathePrueba.java
@@ -11,6 +11,7 @@ package karenaguaysa;
 public class kathePrueba {
     public static void main(String[] args) {
         System.out.println("Hola Katy");
+        System.out.println("xd");
     }
     
 }
